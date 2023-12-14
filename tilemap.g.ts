8aba185cf5e9e7fa27921baf8c230c6079e4e60b
Corentin Bunaux <130914810+corentinbunaux@users.noreply.github.com>
2023-12-14 19:00:02 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ tilemap.g.ts
@@ -0,0 +1,96 @@
+// Code généré automatiquement. Ne pas modifier.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "niveau1":
+            case "niveau1":return tiles.createTilemap(hex`1000100002030303030303030303030303030304000100000000000000000000000000000005000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000006000000000000000000000000000000000700000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.dungeon.chestClosed,sprites.dungeon.darkGroundNorthWest0,sprites.dungeon.darkGroundNorth,sprites.dungeon.darkGroundNorthEast0,sprites.dungeon.collectibleInsignia,sprites.vehicle.roadVertical,sprites.vehicle.roadHorizontal], TileScale.Sixteen);
+            case "niveau2":
+            case "niveau2":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16], TileScale.Sixteen);
+            case "niveau3":
+            case "niveau3":return tiles.createTilemap(hex`1000100001000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.castle.tilePath1], TileScale.Sixteen);
+            case "niveau4":
+            case "niveau4":return tiles.createTilemap(hex`100010000202020502020202020202020202020202020c050c0c0c0c0c0c0c0c0c0c0202020c0e050e010e0e0e0e0e010e0e0c020c0e0e070606090e0e0e0e0e0e080606010e0d0e0e0e050e0d08060606040e0e0e0e0e0e0e01050e0e05010e0e0e0d0d0d0d0d0e0e0c0b06060a0e0e0d0d0e0e050d0d0d0e0c05020c050e0e0d0e010e0706060603060a0c02050e0e0e0d0e0e0e010e0e050e070603040e0e0d0e0d0e0d0e0e0e050e0e01050e0e010d0e0d0e0e080606040e0e0e050e0e0e0d0d0e0e0e050e0e0e0e0d0e070606060606090e0e050e0e0d0e0d0e0e0e0e0e0e0e050e06040e010d0d0d0d0d0d010e0e0e050e0e0e0e0e0e0d0e0d0d0e0e0e0e0e050e`, img`
+. . 2 . 2 . . . . . . . . . . . 
+. . 2 . 2 . . . . . . . . . . . 
+. . 2 . 2 2 2 2 . . . . 2 2 2 2 
+. . 2 . . . . 2 2 2 2 2 2 . . . 
+. . 2 2 2 2 . 2 2 . . . . . 2 2 
+. . . . . 2 . . . . . 2 2 2 2 . 
+2 2 . . 2 . . . . . . 2 . . . . 
+. 2 2 2 2 . . 2 2 . 2 2 . . . . 
+. . . . . . . 2 2 . 2 2 . . . . 
+2 2 2 . . . . . . . 2 2 . . . . 
+2 2 2 2 . . 2 . . . 2 2 . . . . 
+2 . . . . 2 . 2 . 2 2 2 2 2 2 2 
+2 . 2 2 2 2 . 2 . . . . . . . 2 
+2 . 2 . . . . 2 2 2 2 2 2 2 . 2 
+. . 2 . . . . . . . . . . 2 . 2 
+2 2 2 . . . . . . . . . . 2 . 2 
+`, [myTiles.transparency16,sprites.builtin.oceanSand0,sprites.castle.saplingPine,sprites.vehicle.roadIntersection3,sprites.vehicle.roadTurn4,sprites.vehicle.roadVertical,sprites.vehicle.roadHorizontal,sprites.vehicle.roadTurn3,sprites.vehicle.roadTurn1,sprites.vehicle.roadTurn2,sprites.vehicle.roadIntersection4,sprites.vehicle.roadIntersection2,sprites.builtin.coral1,sprites.builtin.coral5,sprites.castle.tileGrass3], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// Code généré automatiquement. Ne pas modifier.
